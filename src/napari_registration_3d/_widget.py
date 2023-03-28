37aa84442831850b11c697e34ebef34e3ee6d710
Tiancheng Liu <tianchengliu.com@gmail.com>
2023-03-28 07:05:43 -0500

    align camera

--- src/napari_registration_3d/_widget.py
+++ src/napari_registration_3d/_widget.py
@@ -268,7 +268,8 @@ class MainWidget(QWidget):
             self.overlay_image_layer.visible = False
 
     def align_viewers_btn_clicked(self):
-        self.src_viewer.camera = self.tgt_viewer.camera
+        self.src_viewer.camera.angles = self.tgt_viewer.camera.angles
+        self.src_viewer.camera.zoom = self.tgt_viewer.camera.zoom
 
     def landmark_list_box_item_current_row_changed(self):
         row = self.landmark_list_box.currentRow()
